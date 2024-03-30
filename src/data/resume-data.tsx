770decfd79b80658ba6592dd8ca23ec46f6cf0cf
Leonardo Vida <lleonardovida@gmail.com>
2024-03-30 18:10:02 +0000

    feat: update resume

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -57,8 +57,15 @@ export const RESUME_DATA = {
       logo: [],
       start: "Dec 2023",
       end: "Present",
-      description:
-        "For a large group in the SME accounting sector, developed the digital transformation strategy with a focus on LLM use cases, managing the relationship with c-level and leading the development of a data platform to enable core and moonshot use cases. Technologies: Python, TypeScript, HCL",
+      description: (
+        <>
+          <li>For client A, developed data platform (Azure and Terraform) to centralize data of 8 subsidiaries companies</li>
+          <li>For client A, created API wrapper templates and used them across 10+ sources alongside custom libraries for ETL pipelines</li>
+          <li>For a client B, developed and productionized advanced custom RAG solution over 1M+ files on Azure used 700+ employees</li>
+          <li>For a client B, finetuned embedding model, reduced LLM cost by 40X, deployed system monitoring and telemetry</li>
+          <li>Mentored junior team members, led trainings and workshops on the application and productionization of LLMs</li>
+        </>
+      ),
     },
     {
       company: "Prima assicurazioni",
@@ -70,20 +77,11 @@ export const RESUME_DATA = {
       end: "Nov 2023",
       description: (
         <>
-          <li>
-            Architected and supported the development of a self-service 
-            data platform based on data mesh principles on AWS, orchestrated 
-            by Argo workflows, using Airbyte for ingestion, and Databricks plus 
-            dbt for compute and transformation. 
-          </li>
-          <li>
-            Developed and optimized batch and 
-            streaming PySpark ETL pipelines, frameworks and libraries (Python and Rust). 
-          </li>
-          <li>
-            Defined data product, contract and permission specifications across the company 
-            and supported roadmap definition.
-          </li>
+        <li>Architected and supported the development of self-service data platform based on data mesh principles on AWS</li>
+        <li>Refactored old in-house config-driven ETL package and developed supporting libraries for self-service data platform</li>
+        <li>Engineered petabyte-scale ETL processes in PySpark focusing on data quality and data pipeline efficiency</li>
+        <li>Implemented agile product management, boosting efficiency and team morale</li>
+        <li>Defined data product, contract and permission specifications across the company and supported team's roadmap definition</li>
         </>
       ),
     },
@@ -97,22 +95,11 @@ export const RESUME_DATA = {
       end: "May 2023",
       description: (
         <>
-          <li>
-            Led a team of five in architecting and developing a data platform
-            using AWS native services and Airflow; managed the team
-            quarterly plannings and led all the rituals.
-          </li>
-          <li>
-            Orchestrated PySpark data pipelines to connect all subsidiaries
-            in EMEA and North America and produce source-oriented foundational
-            Data Products to feed machine learning use cases.
-          </li>
-          <li>
-            Spearheaded development of Python libraries for data processing, 
-            data quality monitoring, and logging (streaming and batch) and 
-            supported the transition to a new global AWS organization 
-            designing company-wide frameworks for all Data Products.
-          </li>
+        <li>Led core Data Engineering team with a total of 6 developers in newly created Data department</li>
+        <li>Architected AWS-based performance data platform with focus on security and data quality</li>
+        <li>Developed all core pipelines for EMEA and NA subsidiaries and main libraries for data processing and quality monitoring</li>
+        <li>Deployed SLAs monitoring for critical data sets, ensuring high data availability and integrity</li>
+        <li>Managed team planning and rituals, and translated business needs into technical requirements prioritizing them</li>
         </>
       ),
     },
@@ -126,21 +113,10 @@ export const RESUME_DATA = {
       end: "Sep 2022",
       description: (
         <>
-          <li>
-            Transitioned core data pipelines in Azure (Databricks and ADF)
-            from batch to real-time streaming (Kafka and Debezium);
-            enhanced data observability library and introduced 
-            automated data tests.
-          </li>
-          <li>
-            Architected and deployed an MLOps platform on MLFlow and 
-            created ML models to forecast churn, LTV and predict demand; 
-            improved demand forecasting accuracy by approximately 40%.
-          </li>
-          <li>
-            Integrated into the existing data infrastructure a new B2B2C 
-            marketplace and direct-to-consumer product, developing
-          </li>
+          <li>Transitioned all core ETL pipelines from batch to real-time streaming</li>
+          <li>Refactored and enhanced data observability library and introduced automated data tests for bronze and silver layers</li>
+          <li>Deployed MLOps platform on MLFlow and integrated into data infrastructure new B2B2C marketplace and D2C product</li>
+          <li>Created ML models to forecast churn, LTV and predict demand, improving demand forecasting accuracy by 40\%</li>
         </>
       ),
     },
@@ -154,17 +130,10 @@ export const RESUME_DATA = {
       end: "Jul 2021",
       description: (
         <>
-          <li>
-          Led the engineering of a research project, finetuned transformers models 
-          with €100k GCP grant and created historicized models, created API to extract 
-          the entire Delpher collection of the Dutch national library, developed 
-          ETL pipelines to extract, OCR, chunk and score texts.
-          </li>
-          <li>
-          Developed back-end security of OSS to automate systematic reviews (asreview)
-          having more than 140,000 downloads on PyPi and OSS spatial data 
-          package (osmenrich) for sensitive data enrichment.
-          </li>
+          <li>Led research project engineering, fine-tuned transformer models with \$100k+ GCP grant</li>
+          <li>Developed custom pipeline to extract entire collection of the Dutch national library, process, OCR and score texts</li>
+          <li>Developed back-end security of OSS to automate systematic reviews (asreview), with more than 150,000 downloads on PyPi</li>
+          <li>Developed OSS spatial data package (osmenrich) in R for sensitive data enrichment</li>
         </>
       ),
     },
@@ -189,7 +158,7 @@ export const RESUME_DATA = {
         "OpenAI",
         "Custom LLM", 
       ],
-      description: "A copilot for companies to improve the time to best talent for a given position. It uses a LLM pipeline to understand the job description, resume and company, and then it scores candidates, write rejection/ confirmation emails and much more, all under human supervision.",
+      description: "A LLM-based autopilot for companies to improve the time to best talent for a given position. It uses a custom LLM pipeline to understand the job description, resume and company, and then it scores candidates, write rejection/ confirmation emails and much more, all under human supervision.",
       logo: [],
       link: {
         label: "talentcopilot",
