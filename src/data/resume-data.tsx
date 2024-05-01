eeb1bbbd9dbe3c7b2e565b7685d997ba28d09365
Leonardo Vida <lleonardovida@gmail.com>
2024-05-01 18:00:46 +0200

    feat: update resume

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -59,11 +59,14 @@ export const RESUME_DATA = {
       end: "Present",
       description: (
         <>
-          <li>For client A, developed data platform (Azure and Terraform) to centralize data of 8 subsidiaries companies</li>
-          <li>For client A, created API wrapper templates and used them across 10+ sources alongside custom libraries for ETL pipelines</li>
-          <li>For a client B, developed and productionized advanced custom RAG solution over 1M+ files on Azure used 700+ employees</li>
-          <li>For a client B, finetuned embedding model, reduced LLM cost by 40X, deployed system monitoring and telemetry</li>
-          <li>Mentored junior team members, led trainings and workshops on the application and productionization of LLMs</li>
+          <li>For client A, deployed data platform using IaC on Azure (Databricks, dbt) to centralize data of growing number of subsidiaries</li>
+          <li>For client A, created custom API wrappers to ELT across 10+ sources alongside custom libraries for pipelines and data tests</li>
+          <li>For client B, developed and productionized advanced custom RAG solution over 1M+ files on Azure used by 700+ employees</li>
+          <li>For client B, deployed self-hosted LLM reducing cost by 40X, trace monitoring and system telemetry (Langsmith, Azure)</li>
+          <li>For client C, deployed data platform using IaC on AWS (Airbyte, Databricks and Dagster), provided infra to backend team</li>
+          <li>Developed internal terraform blueprints and modules for a range of data platform solutions and client sizes</li>
+          <li>Led the development of internal project for a co-pilot for recruitment, currently being tested at a partner company</li>
+          <li>Mentored junior team members, led trainings and clients' workshops on the application and productionization of LLMs</li>
         </>
       ),
     },
@@ -77,11 +80,11 @@ export const RESUME_DATA = {
       end: "Nov 2023",
       description: (
         <>
-        <li>Architected and supported the development of self-service data platform based on data mesh principles on AWS</li>
+        <li>Supported the development of self-service data platform based on data mesh principles on AWS (dbt, Airbyte, Argo)</li>
         <li>Refactored old in-house config-driven ETL package and developed supporting libraries for self-service data platform</li>
         <li>Engineered petabyte-scale ETL processes in PySpark focusing on data quality and data pipeline efficiency</li>
         <li>Implemented agile product management, boosting efficiency and team morale</li>
-        <li>Defined data product, contract and permission specifications across the company and supported team&apos;s roadmap definition</li>
+        <li>Defined data product, contract and permission specifications across the company and supported team's roadmap definition</li>
         </>
       ),
     },
@@ -96,7 +99,7 @@ export const RESUME_DATA = {
       description: (
         <>
         <li>Led core Data Engineering team with a total of 6 developers in newly created Data department</li>
-        <li>Architected AWS-based performance data platform with focus on security and data quality</li>
+        <li>Architected and developed AWS-native data platform with focus on security and data quality (Glue, Iceberg, MWAA)</li>
         <li>Developed all core pipelines for EMEA and NA subsidiaries and main libraries for data processing and quality monitoring</li>
         <li>Deployed SLAs monitoring for critical data sets, ensuring high data availability and integrity</li>
         <li>Managed team planning and rituals, and translated business needs into technical requirements prioritizing them</li>
@@ -113,8 +116,8 @@ export const RESUME_DATA = {
       end: "Sep 2022",
       description: (
         <>
-          <li>Transitioned all core ETL pipelines from batch to real-time streaming</li>
-          <li>Refactored and enhanced data observability library and introduced automated data tests for bronze and silver layers</li>
+          <li>Transitioned all core ETL pipelines from batch to micro-batch structured streaming and then DLT on Databricks</li>
+          <li>Refactored and enhanced data observability library and introduced automated data tests across all medallion layers</li>
           <li>Deployed MLOps platform on MLFlow and integrated into data infrastructure new B2B2C marketplace and D2C product</li>
           <li>Created ML models to forecast churn, LTV and predict demand, improving demand forecasting accuracy by 40\%</li>
         </>
@@ -132,8 +135,8 @@ export const RESUME_DATA = {
         <>
           <li>Led research project engineering, fine-tuned transformer models with \$100k+ GCP grant</li>
           <li>Developed custom pipeline to extract entire collection of the Dutch national library, process, OCR and score texts</li>
-          <li>Developed back-end security of OSS to automate systematic reviews (asreview), with more than 150,000 downloads on PyPi</li>
-          <li>Developed OSS spatial data package (osmenrich) in R for sensitive data enrichment</li>
+          <li>Developed back-end security of OSS to automate systematic reviews (<a href="https://github.com/asreview/asreview">asreview</a>), with more than 150,000 downloads on PyPi</li>
+          <li>Developed OSS spatial data package (<a href="https://github.com/sodascience/osmenrich">osmenrich</a>) in R for sensitive data enrichment</li>
         </>
       ),
     },
@@ -146,6 +149,7 @@ export const RESUME_DATA = {
     "Terraform HCL/Docker/Kubernetes",
     "GraphQL",
     "R",
+    "Langchain, llamaindex, open source LLMs"
   ],
   projects: [
     {
@@ -155,8 +159,9 @@ export const RESUME_DATA = {
         "TypeScript",
         "Next.js",
         "Python",
-        "OpenAI",
-        "Custom LLM", 
+        "FastAPI",
+        "Openrouter",
+        "LLM", 
       ],
       description: "A LLM-based autopilot for companies to improve the time to best talent for a given position. It uses a custom LLM pipeline to understand the job description, resume and company, and then it scores candidates, write rejection/ confirmation emails and much more, all under human supervision.",
       logo: [],
